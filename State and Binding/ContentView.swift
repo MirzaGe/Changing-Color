7b2e97ac334d776b8abe8d488a88646e660baa9e
Sherry1481 <shareyscorpi1@gmail.com>
2021-07-22 13:48:43 +0500

    done challenge

--- State and Binding/ContentView.swift
+++ State and Binding/ContentView.swift
@@ -33,17 +33,20 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State private var swiftyColor: Color = .red
+    @State private var swiftyOpacity: Double = 0.7
 
   var body: some View {
     VStack {
-        ColorPicker("Swifty Color", selection: /*@START_MENU_TOKEN@*/.constant(.red)/*@END_MENU_TOKEN@*/)
+        SwiftyControls(swiftyColor: $swiftyColor, swiftyOpacity: $swiftyOpacity)
+       
       Image(systemName: "swift")
         .resizable()
         .scaledToFit()
         .padding(25)
         .foregroundColor(.white)
-        .opacity(0.7)
-        .background(Color.black)
+        .opacity(swiftyOpacity)
+        .background(swiftyColor)
         .cornerRadius(50)
     }
     .padding()
@@ -54,4 +57,16 @@ struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
   }
+}
+
+struct SwiftyControls: View {
+    @Binding var swiftyColor: Color
+     @Binding var swiftyOpacity: Double
+    var body: some View {
+        VStack {
+            ColorPicker("Swifty Color", selection: $swiftyColor)
+            Slider(value: $swiftyOpacity, in: 0...1)
+                .accentColor(swiftyColor)
+        }
+    }
 }
